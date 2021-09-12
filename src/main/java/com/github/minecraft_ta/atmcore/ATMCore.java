51d8e2f5333f7e5a91872702b86604e2fd14ce07
Pelotrio <45769595+Pelotrio@users.noreply.github.com>
2021-09-12 19:01:56 +0200

    Deleted testing stuff and updated gradle and forgegradle

--- src/main/java/com/github/minecraft_ta/atmcore/ATMCore.java
+++ src/main/java/com/github/minecraft_ta/atmcore/ATMCore.java
@@ -1,32 +1,7 @@
 package com.github.minecraft_ta.atmcore;
 
-import com.github.minecraft_ta.atmcore.block.RandomBlock;
-import com.github.minecraft_ta.atmcore.block.RandomBlockTile;
-import com.github.minecraft_ta.atmcore.implementation.RandomCounter;
-import net.minecraft.block.Block;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.text.Style;
-import net.minecraft.util.text.TextComponentString;
-import net.minecraft.util.text.TextFormatting;
-import net.minecraft.world.World;
-import net.minecraft.world.WorldServer;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
 import org.apache.logging.log4j.Logger;
-
-import java.util.Objects;
-import java.util.Random;
 
 
 @Mod(
@@ -45,73 +20,5 @@ public class ATMCore {
     public static ATMCore INSTANCE;
 
     public static Logger LOGGER;
-    private long timeStamp;
 
-    @Mod.EventHandler
-    public void onPreInit(FMLPreInitializationEvent event) {
-        LOGGER = event.getModLog();
-        GameRegistry.registerTileEntity(RandomBlockTile.class, new ResourceLocation(MOD_ID, "random_block_tile"));
-    }
-
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    @SubscribeEvent
-    public void onServerTick(TickEvent.WorldTickEvent event) {
-  /*      if (event.side.isServer()) {
-            World world = event.world;
-            Random rand = world.rand;
-            if ((System.currentTimeMillis() - timeStamp) >= 1000) {
-                if (rand instanceof RandomCounter) {
-                  *//*  int counter = ((RandomCounter) rand).getCounter();
-                    world.playerEntities.forEach(entityPlayer ->
-                            entityPlayer.sendMessage(new TextComponentString("Random calls in " + world.provider.getDimensionType().getName() + ": ").setStyle(new Style().setColor(TextFormatting.AQUA))
-                                    .appendSibling(new TextComponentString(counter + "").setStyle(new Style().setColor(TextFormatting.GOLD)))));
-                    LOGGER.info("Random calls: " + counter);
-                    ((RandomCounter) rand).setCounter(0);*//*
-
-                    for (WorldServer worldServer : Objects.requireNonNull(world.getMinecraftServer()).worlds) {
-                        LOGGER.info(worldServer.provider.getDimensionType().getName() + " " + worldServer.provider.getDimensionType().getId());
-
-                    }
-                }
-                timeStamp = System.currentTimeMillis();
-            }
-        }*/
-    }
-
-    @GameRegistry.ObjectHolder(MOD_ID)
-    public static class Blocks {
-        public static final RandomBlock RANDOM_BLOCK = null;
-    }
-
-    @GameRegistry.ObjectHolder(MOD_ID)
-    public static class Items {
-        public static final ItemBlock RANDOM_BLOCK_ITEM = null;
-    }
-
-    @Mod.EventBusSubscriber
-    public static class ObjectRegistryHandler {
-
-        @SubscribeEvent
-        public static void addItems(RegistryEvent.Register<Item> event) {
-            event.getRegistry().register(new ItemBlock(Blocks.RANDOM_BLOCK).setRegistryName(Blocks.RANDOM_BLOCK.getRegistryName()));
-        }
-
-        @SubscribeEvent
-        public static void addBlocks(RegistryEvent.Register<Block> event) {
-            event.getRegistry().register(new RandomBlock().setRegistryName(new ResourceLocation(MOD_ID, "random_block")));
-        }
-
-        @SubscribeEvent
-        public static void registerRenders(ModelRegistryEvent event) {
-            registerRender(Item.getItemFromBlock(Blocks.RANDOM_BLOCK));
-        }
-
-        public static void registerRender(Item item) {
-            ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), "inventory"));
-        }
-    }
 }
--- src/main/java/com/github/minecraft_ta/atmcore/core/CoreMod.java
+++ src/main/java/com/github/minecraft_ta/atmcore/core/CoreMod.java
@@ -1,20 +1,18 @@
 package com.github.minecraft_ta.atmcore.core;
 
+import com.github.minecraft_ta.atmcore.ATMCore;
 import net.minecraftforge.common.ForgeVersion;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.MixinEnvironment;
 import org.spongepowered.asm.mixin.Mixins;
 
 import javax.annotation.Nullable;
 import java.util.Map;
 
+@IFMLLoadingPlugin.Name("Total Performance")
 @IFMLLoadingPlugin.MCVersion(ForgeVersion.mcVersion)
 public class CoreMod implements IFMLLoadingPlugin {
-
-    public CoreMod() {
-        MixinBootstrap.init();
-        Mixins.addConfiguration("mixins.atm_core.json");
-    }
 
     @Override
     public String[] getASMTransformerClass() {
@@ -34,12 +32,14 @@ public class CoreMod implements IFMLLoadingPlugin {
 
     @Override
     public void injectData(Map<String, Object> data) {
-
+        MixinBootstrap.init();
+        Mixins.addConfiguration("mixins." + ATMCore.MOD_ID + ".json");
+        MixinEnvironment.getDefaultEnvironment().setObfuscationContext("searge");
     }
 
     @Override
     public String getAccessTransformerClass() {
         return null;
     }
-
 }
+
